41104c21eafc5f899f18a59d3b5fe7dc0c3d95bd
Adi <a1bhatna@ucsd.edu>
2018-05-06 23:18:59 -0700

    Helper code for Gmaps

---
+++ SMAP/SMAP/ViewModels/DashboardPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Prism.Navigation;
+
+namespace SMAP.ViewModels
+{
+    public class DashboardPageViewModel : INavigatedAware
+    {
+        INavigationService _navigationService;
+
+        public DashboardPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SMAP/SMAP/Views/DashboardPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+using System.Collections.Generic;
+using Xamarin.Forms.GoogleMaps;
+
+using Xamarin.Forms;
+
+namespace SMAP.Views
+{
+    public partial class DashboardPage : ContentPage
+    {
+
+        public DashboardPage()
+        {
+            InitializeComponent();
+
+            map.MapStyle = MapStyle.FromJson(getMapStyleJson());
+        }
+
+        public string getMapStyleJson(){
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(DashboardPage)).Assembly;
+            System.IO.Stream stream = assembly.GetManifestResourceStream("SMAP.Helpers.GoogleMapsStyle.json");
+            string text = "";
+            using (var reader = new System.IO.StreamReader(stream))
+            {
+                text = reader.ReadToEnd();
+            }
+
+            return text;
+        }
+    }
+}
