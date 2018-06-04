f005cd47339bdaa3c8a6eb4510a68db0ccfdab5d
Adi <a1bhatna@ucsd.edu>
2018-06-03 17:40:20 -0700

    settings plugin initialized

--- SMAP/SMAP/App.xaml.cs
+++ SMAP/SMAP/App.xaml.cs
@@ -19,6 +19,8 @@ namespace SMAP
         public App(IPlatformInitializer initializer = null) : base(initializer) 
         {
             InitializeComponent();
+
+            //START PAGE (Add login checks?)
             NavigationService.NavigateAsync("DashboardPage");
         }
 
