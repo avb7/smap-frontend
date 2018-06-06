a40237d525272195f754126d0cd8d5cb50a74cda
christopherthomas226 <cat044@ucsd.edu>
2018-06-06 12:36:46 -0700

    Updated Constants

--- SMAP/SMAP/Helpers/Constants.cs
+++ SMAP/SMAP/Helpers/Constants.cs
@@ -17,5 +17,11 @@ namespace SMAP.Helpers
         public static string iOSRedirectUrl = "https://www.facebook.com/connect/login_success.html";
         public static string AndroidRedirectUrl = "https://www.facebook.com/connect/login_success.html";
 
+        // SMAP API URL
+        // Base URL for SMAP API
+        public static string BaseURL = "https://ss6aagzajf.execute-api.us-east-2.amazonaws.com/stage_1";
+        public static string APIKey = "xf6s4nO2G85qUbLYdE6FK74sZuYbPbEF4NqppdGZ";
+
+
     }
 }
