7e1cc36ee371216408dc24e0a1ba23257d5e14e1
Sevag <skeossei@ucsd.edu>
2018-06-08 15:50:13 -0700

    Forms page

---
+++ SMAP/SMAP/Views/Forms.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SMAP.Views
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
