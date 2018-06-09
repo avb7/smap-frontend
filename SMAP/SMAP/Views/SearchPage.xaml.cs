c91ed1eef749ab79ca2df89a231a227deab13824
Sevag <skeossei@ucsd.edu>
2018-06-08 20:51:19 -0700

    Search Page

---
+++ SMAP/SMAP/Views/SearchPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SMAP.Views
+{
+    public partial class SearchPage : ContentPage
+    {
+        public SearchPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
