b5c92c352b425971764918a1a6312bcd9dbcdc6f
Sevag <skeossei@ucsd.edu>
2018-06-08 18:38:54 -0700

    Add friend page

---
+++ SMAP/SMAP/Views/AddFriendPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SMAP.Views
+{
+    public partial class AddFriendPage : ContentPage
+    {
+        public AddFriendPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
